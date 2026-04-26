b2d8088afcf201cfa48956c87118869940ad90e1
Alex <kaunitzalex@gmail.com>
2026-04-27 01:53:22 +0300

    temple

--- Assets/Abstraction/Character.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-public class Character : MonoBehaviour
-{
-    public virtual void Act()
-    {
-        Debug.Log("Character acts");
-    }
-}
--- Assets/Abstraction/Player.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Player : Character
-{
-    public override void Act()
-    {
-        Debug.Log("Player moves");
-    }
-}
